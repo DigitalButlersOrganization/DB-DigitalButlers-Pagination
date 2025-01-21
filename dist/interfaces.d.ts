c039b878f1bf104b6a8bca638695aacb60dc457d
Sasha Afanasienka <afanasienka@yandex.by>
2025-01-21 13:50:32 +0300

    feat: change ver

--- dist/interfaces.d.ts
+++ dist/interfaces.d.ts
@@ -1,8 +1,8 @@
 import { Pagination } from './index';
 export interface EventModel {
-    afterInit?: (pagination: Pagination) => {};
-    change?: (pagination: Pagination) => {};
-    click?: (pagination: Pagination) => {};
+    afterInit?: (pagination: Pagination) => void;
+    change?: (pagination: Pagination) => void;
+    click?: (pagination: Pagination) => void;
 }
 export type PageNumberTransformer = (number: number) => string;
 export interface PaginationConfigModel {
